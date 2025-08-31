71b8347b3de324f73cb8acfcf30e3204814bc507
DanielyVasconcelos2024 <daniely.vasconcelos@academico.uncisal.edu.br>
2025-08-30 23:17:26 -0300

    study: exercicios do livro Java: Como programar, por Deitel 8ª edição, capitulo 2

---
+++ 01-fundamentos/src/deitel/exercicios/MaiorMenor.java
@@ -0,0 +1,76 @@
+package deitel.exercicios;
+import java.util.Scanner;
+
+/*Enunciado : (Maiores e menores inteiros) Escreva um aplicativo que lê cinco inteiros, determina e imprime o maior e o menor inteiro no grupo. Utilize 
+somente as técnicas de programação que você aprendeu neste capítulo. */
+
+public class MaiorMenor {
+    public static void main(String[] args){
+        System.out.print("Insira o primeiro numero : ");
+
+
+        Scanner entrada1 = new Scanner(System.in);
+        int numero1 = entrada1.nextInt();
+        System.out.printf("%d\n", numero1);
+
+        int maior;
+        int menor;
+
+
+        maior = numero1;
+        menor = numero1; 
+        
+        System.out.print("Insira o segundo numero:");
+        
+        int numero2= entrada1.nextInt();
+        System.out.printf("%d\n", numero2);
+        if (numero2 > maior) {
+            maior = numero2;           
+        }
+        if (numero2 < menor) {
+             menor = numero2;
+        }
+
+        System.out.print("Insira o terceiro numero:");
+        int numero3= entrada1.nextInt();
+        System.out.printf("%d\n", numero3);
+
+        if (numero3 > maior) {
+            maior = numero3;           
+        }
+        if (numero3 < menor) {
+             menor = numero3;
+        }
+
+        System.out.print("Insira o quarto numero:");
+        int numero4= entrada1.nextInt();
+        System.out.printf("%d\n", numero4);
+
+        if (numero4 > maior) {
+            maior = numero4;           
+        }
+        if (numero4 < menor) {
+             menor = numero4;
+        }
+
+        System.out.print("Insira o quinto numero:");
+        int numero5= entrada1.nextInt();
+        System.out.printf("%d\n", numero5);
+
+        if (numero5 > maior) {
+            maior = numero5;           
+        }
+        if (numero5 < menor) {
+             menor = numero5;
+        }
+
+         System.out.print("O numero maior é :");
+         System.out.printf("%d\n", maior);
+
+         System.out.print("O numero menor é :");
+         System.out.printf("%d\n", menor);
+
+    }
+    
+}
+// depois refazer esse exercicio usando o laço for .
---
+++ 01-fundamentos/src/deitel/exercicios/Produto.java
@@ -0,0 +1,36 @@
+package deitel.exercicios;
+
+import java.util.Scanner;
+// a) Declare que um programa calculará o produto de três inteiros.
+public class Produto {
+    public static void main(String[] args){
+        // b) Crie um Scanner chamado input que lê valores a partir da entrada padrão.
+Scanner entrada = new Scanner(System.in);
+// c) Declare as variáveis x, y, z e result como tipo int.
+int x;
+int y;
+int z;
+int result;
+// d) Solicite que o usuário insira o primeiro inteiro.
+    System.out.println("Insira o valor de x: ");
+// e) Leia o primeiro inteiro digitado pelo usuário e armazene-o na variável x.
+    x = entrada.nextInt();
+
+// f) Solicite que o usuário insira o segundo inteiro.
+    System.out.println("Insira o valor de y: ");    
+// g) Leia o segundo inteiro digitado pelo usuário e armazene-o na variável y.
+    y = entrada.nextInt();
+
+// h) Solicite que o usuário insira o terceiro inteiro.
+    System.out.println("Insira o valor de z: ");  
+// i) Leia o terceiro inteiro digitado pelo usuário e armazene-o na variável z.
+    z = entrada.nextInt();
+
+// j) Compute o produto dos três inteiros contidos nas variáveis x, y e z e atribua o resultado à variável result.
+    result = x*y*z;
+// k) Exiba a mensagem "Product is" seguida pelo valor da variável result */
+    System.out.print("O produto de dos valores x, y e z é : ");
+    System.out.printf("%d\n", result);
+}
+
+}
